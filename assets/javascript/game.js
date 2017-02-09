682050c0d4f730c80dbd5196aef1c01bee4c10de
Matthew Jakobovic <mpjakobovic@gmail.com>
2017-02-08 23:02:54 -0500

    working code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,30 +28,69 @@ $(document).ready(function(){
 	//Display Player score
 	$('#player-score').html('<h1>' + playerScore + '</h1>');
 
-	//Player win condition
-	if (playerScore === targetScore) {
-		win++;
-		$('wins').text("Wins: " + win);
-		alert("You won!")
-		reset();
-	} 
-	//Player lose conditon
-	else if (playerScore > targetScore) {
-		lose++;
-		$('lose').text("Losses: " + lose);
-		alert("You lost. Try again.");
-		reset();
+	
+
+	for (var i = 1; i < 5; i++) {
+		var gems = $('<img>');
+		gems.addClass("crystal-image");
+		gems.attr("src", "assets/images/" + i + ".png");
+		gems.attr("value", Math.round(Math.random()*(12 - 1)) + 1);
+		$('#crystals').append(gems);
+
 	}
 
+	$('.crystal-image').on("click", function(){
+		var crystalPoints = ($(this).attr("value"));
+		crystalPoints = parseInt(crystalPoints);
+		playerScore += crystalPoints;
+		score();
+		console.log($(this).attr("value"));
+		console.log(playerScore);
+
+		//Player win condition
+		if (playerScore === targetScore) {
+		winner();
+		reset();
+		console.log(playerScore);
+		console.log(targetScore);
+		} 
+		//Player lose conditon
+		if (playerScore > targetScore) {
+		loser();
+		console.log("Before reset " + playerScore);
+		console.log("Before reset " + targetScore);
+		reset();
+		console.log("After reset() playerScore " + playerScore);
+		console.log("After reset() targetScore " + targetScore);
+		}
+	});
+
+	
 //reset the game	
 function reset(){
-	$('#player-score').empty();
-	var playerScore = 0;
-	var targetScore = Math.round(Math.random() * (120 - 19)) + 19;
-	var crystalValue = Math.round(Math.random() * (12 - 1)) + 1;
+	playerScore = 0;
+	$('#player-score').html('<h1>' + playerScore + '</h1>');
+	targetScore = Math.round(Math.random() * (120 - 19)) + 19;
+	crystalValue = Math.round(Math.random() * (12 - 1)) + 1;
+	$('#target-score').html('<h1>' + targetScore + '</h1>');
 }
 
-});
-for (var i = 1; i < 5; i++) {
-	$('#crystal-image').attr("value", crystalValue);
+function score() {
+	$('#player-score').html('<h1>' + playerScore + '</h1>');
 }
+
+function winner() {
+	win++;
+	$('#wins').text("Wins: " + win);
+	alert("You won!");
+	
+}
+
+function loser() {
+	lose++;
+	$('#lose').text("Losses: " + lose);
+	alert("You lost. Try again.");
+	console.log("Lose score: " + playerScore);
+}
+});
+
